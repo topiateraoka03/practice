11e3e9174af710d307f2f77fc20e2a5befc70f4a
topiateraoka03 <147683817+topiateraoka03@users.noreply.github.com>
2024-10-17 15:26:15 +0900

    Update Book.java

--- Book.java
+++ Book.java
@@ -4,5 +4,5 @@ public Book(String name,int price,String color,String isbn){
 	super(name,price,color);
 	this.isbn = isbn;
 }
-public String getIs(){return this.isbn;}
+public String getIsbn(){return this.isbn;}
 }
